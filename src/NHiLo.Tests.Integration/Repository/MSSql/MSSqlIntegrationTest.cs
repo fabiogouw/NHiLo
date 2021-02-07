2e1453f86461ac7f624827a434ae840f8701e563
Fabio Gouw <fabiogouw@gmail.com>
2021-02-06 23:28:43 -0300

    Issue #1

--- src/NHiLo.Tests.Integration/Repository/MSSql/MSSqlIntegrationTest.cs
+++ src/NHiLo.Tests.Integration/Repository/MSSql/MSSqlIntegrationTest.cs
@@ -1,11 +1,14 @@
 using DotNet.Testcontainers.Containers.Builders;
+using DotNet.Testcontainers.Containers.Configurations.Databases;
 using DotNet.Testcontainers.Containers.Modules;
+using DotNet.Testcontainers.Containers.Modules.Databases;
 using DotNet.Testcontainers.Containers.WaitStrategies;
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.IO;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
@@ -107,6 +110,172 @@ namespace NHiLo.Tests.Integration.Repository.MSSql
             long nexttHi = validateNextHi(cmd);
             _output.WriteLine($"Next Hi value: '{nexttHi}'");
             Assert.True(nexttHi == 2, "Expected next Hi value to be equal to 2 (first execution).");
+        }
+
+        [Fact]
+        [Trait("Category", "Integration")]
+        public async void ShouldRaiseErrorForLackOfSELECTPermission()
+        {
+            var testcontainersBuilder = new TestcontainersBuilder<MsSqlTestcontainer>()
+                .WithDatabase(new MsSqlTestcontainerConfiguration
+                {
+                    Password = "myP@ssword100",
+                });
+
+            await using var testcontainer = testcontainersBuilder.Build();
+            await testcontainer.StartAsync();
+
+            string prepareDbScript = @"
+                sp_configure 'show advanced options',1
+                GO
+                RECONFIGURE WITH OVERRIDE
+                GO
+                sp_configure 'contained database authentication', 1
+                GO
+                RECONFIGURE WITH OVERRIDE
+                GO
+
+                CREATE DATABASE [testDB] CONTAINMENT = PARTIAL;
+                GO
+
+                CREATE LOGIN [nhilo_user] WITH PASSWORD = 'nhilo_p@ssW0rd';
+                GO
+
+                USE [testDB];
+                GO
+
+                CREATE USER [nhilo_user] FOR LOGIN [nhilo_user];
+                GO
+
+                CREATE TABLE [dbo].[NHILO](
+                [ENTITY] [nvarchar](100) NOT NULL,
+                [NEXT_HI] [bigint] NOT NULL,
+                    CONSTRAINT [PK_NHILO] PRIMARY KEY CLUSTERED 
+                    (
+                        [ENTITY] ASC
+                    )
+                );
+                GO
+
+                DENY SELECT ON [dbo].[NHILO] TO [nhilo_user];
+                GO
+            ";
+            await ExecuteScript(prepareDbScript, 
+                new SqlConnection($"Server={ testcontainer.Hostname },{ testcontainer.Port };Database=master;User Id=sa;Password=myP@ssword100;"));
+
+            var builder = new ConfigurationBuilder();
+            builder.AddJsonStream(new MemoryStream(Encoding.UTF8.GetBytes(
+                $@"{{
+                    ""NHiLo"":{{
+                        ""DefaultMaxLo"" : ""100""
+                    }},
+                    ""ConnectionStrings"":{{
+                        ""NHiLo"":{{
+                            ""ConnectionString"":""Server={ testcontainer.Hostname },{ testcontainer.Port };Database=testDB;User Id=nhilo_user;Password=nhilo_p@ssW0rd;"",
+                            ""ProviderName"":""Microsoft.Data.SqlClient""
+                        }}
+                    }}
+                }}"
+                )));
+            var factory = new HiLoGeneratorFactory(builder.Build());
+            try
+            {
+                factory.GetKeyGenerator("myMSSqlSequenceEntity");
+                Assert.True(false, "Expected exception has not been thrown.");
+            }
+            catch (NHiLoException ex)
+            {
+                _output.WriteLine($"InnerException: '{ex.InnerException.Message}'");
+                Assert.Equal(229, (ex.InnerException as SqlException).Number);
+            }
+        }
+
+        [Fact]
+        [Trait("Category", "Integration")]
+        public async void ShouldRaiseErrorForLackOfCREATE_TABLEPermission()
+        {
+            var testcontainersBuilder = new TestcontainersBuilder<MsSqlTestcontainer>()
+                .WithDatabase(new MsSqlTestcontainerConfiguration
+                {
+                    Password = "myP@ssword100",
+                });
+
+            await using var testcontainer = testcontainersBuilder.Build();
+            await testcontainer.StartAsync();
+
+            string prepareDbScript = @"
+                sp_configure 'show advanced options',1
+                GO
+                RECONFIGURE WITH OVERRIDE
+                GO
+                sp_configure 'contained database authentication', 1
+                GO
+                RECONFIGURE WITH OVERRIDE
+                GO
+
+                CREATE DATABASE [testDB] CONTAINMENT = PARTIAL;
+                GO
+
+                CREATE LOGIN [nhilo_user] WITH PASSWORD = 'nhilo_p@ssW0rd';
+                GO
+
+                USE [testDB];
+                GO
+
+                CREATE USER [nhilo_user] FOR LOGIN [nhilo_user];
+                GO
+            ";
+            await ExecuteScript(prepareDbScript,
+                new SqlConnection($"Server={ testcontainer.Hostname },{ testcontainer.Port };Database=master;User Id=sa;Password=myP@ssword100;"));
+
+            var builder = new ConfigurationBuilder();
+            builder.AddJsonStream(new MemoryStream(Encoding.UTF8.GetBytes(
+                $@"{{
+                    ""NHiLo"":{{
+                        ""DefaultMaxLo"" : ""100""
+                    }},
+                    ""ConnectionStrings"":{{
+                        ""NHiLo"":{{
+                            ""ConnectionString"":""Server={ testcontainer.Hostname },{ testcontainer.Port };Database=testDB;User Id=nhilo_user;Password=nhilo_p@ssW0rd;"",
+                            ""ProviderName"":""Microsoft.Data.SqlClient""
+                        }}
+                    }}
+                }}"
+                )));
+            var factory = new HiLoGeneratorFactory(builder.Build());
+            try
+            {
+                factory.GetKeyGenerator("myMSSqlSequenceEntity");
+                Assert.True(false, "Expected exception has not been thrown.");
+            }
+            catch (NHiLoException ex)
+            {
+                _output.WriteLine($"InnerException: '{ex.InnerException.Message}'");
+                Assert.Equal(262, (ex.InnerException as SqlException).Number);
+            }
+        }
+
+        private async Task ExecuteScript(string prepareDbScript, SqlConnection connection)
+        {
+            var commandStrings = Regex.Split(prepareDbScript, @"^\s*GO\s*$", RegexOptions.Multiline | RegexOptions.IgnoreCase);
+            connection.Open();
+            try
+            {
+                foreach (string commandString in commandStrings)
+                {
+                    if (!string.IsNullOrWhiteSpace(commandString.Trim()))
+                    {
+                        using (var command = new SqlCommand(commandString, connection))
+                        {
+                            await command.ExecuteNonQueryAsync();
+                        }
+                    }
+                }
+            }
+            finally
+            {
+                connection.Close();
+            }
         }
     }
 }
