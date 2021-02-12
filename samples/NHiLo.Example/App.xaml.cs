04c130ec7a34f3c0a553ebcd65f56c3d84752716
Fabio Gouw <fabiogouw@gmail.com>
2021-02-12 14:02:35 -0300

    Adding old WPF sample (not working yet)

---
+++ samples/NHiLo.Example/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace NHiLo.Example
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, true);
+            var serviceCollection = ConfigureServices(new ServiceCollection());
+            ServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
+            var mainWindow = serviceProvider.GetService<MainWindow>();
+            mainWindow.Show();
+            
+        }
+
+        private IServiceCollection ConfigureServices(IServiceCollection services)
+        {
+            //services.AddSingleton<ITextService>(provider => new TextService("Hi WPF .NET Core 3.0!"));
+            services.AddSingleton<MainWindow>();
+            return services;
+        }
+    }
+}
---
+++ samples/NHiLo.Example/Model/Contact.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NHiLo.Example.Model
+{
+    public class Contact
+    {
+        public string Id { get; set; }
+        public Person Person { get; set; }
+        public string TelephoneNumber { get; set; }
+    }
+}
