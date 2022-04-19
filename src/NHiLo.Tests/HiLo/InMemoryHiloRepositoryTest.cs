29e08ae72ded284d62221cda4e088085fdc648b1
Fabio Gouw <fabiogouw@gmail.com>
2022-04-18 22:52:38 -0300

    Adding factory and in memory tests

---
+++ src/NHiLo.Tests/HiLo/InMemoryHiloRepositoryTest.cs
@@ -0,0 +1,21 @@
+﻿using NHiLo.Guid;
+using Xunit;
+using FluentAssertions;
+using NHiLo.HiLo.Repository;
+
+namespace NHiLo.Tests.HiLo
+{
+    public class InMemoryHiloRepositoryTest
+    {
+        public class GetNextHi
+        {
+            [Fact]
+            public void ShouldReturnTheNextHiValue()
+            {
+                var sut = new InMemoryHiloRepository();
+                sut.PrepareRepository("myEntity");
+                sut.GetNextHi().Should().Be(1);
+            }
+        }
+    }
+}
