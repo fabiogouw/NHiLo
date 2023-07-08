70dc64efb9e61a79eb4e55552843d92d76f802fa
Fabio Gouw <fabiogouw@gmail.com>
2023-07-08 15:31:05 -0300

    changing libs

--- src/NHiLo/HiLo/Repository/MySqlHiLoRepository.cs
+++ src/NHiLo/HiLo/Repository/MySqlHiLoRepository.cs
@@ -11,7 +11,7 @@ namespace NHiLo.HiLo.Repository
         private string _sqlStatementToInitializeEntity;
 
         public MySqlHiLoRepository(IHiLoConfiguration config)
-            : base(config, MySql.Data.MySqlClient.MySqlClientFactory.Instance)
+            : base(config, MySqlConnector.MySqlConnectorFactory.Instance)
         {
             InitializeSqlStatements(config);
         }
